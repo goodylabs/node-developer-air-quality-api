23cd89ce932c64cb2121942a407131898755487b
Adikso <adam@zambrzycki.net>
2020-05-25 16:43:33 +0200

    Add tests

--- config/app.js
+++ config/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
   port: process.env.PORT || 3000,
   mongodbUri: process.env.MONGODB_URI,
+  mongodbTestUri: process.env.MONGODB_TEST_URI,
   airApiBase: process.env.AIR_API_BASE || 'http://api.gios.gov.pl/pjp-api/rest',
   cronExpression: process.env.CRON_EXPRESSION || '15 * * * *',
 };
