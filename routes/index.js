01037a2dd581a5acbb6b2bac007b342aa2e4176e
Adikso <adam@zambrzycki.net>
2020-05-25 14:30:30 +0200

    Change 'all' endpoint path

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const controllers = require('../controllers/stations.controller');
 
-router.get('/', controllers.all);
+router.get('/stations', controllers.all);
 
 module.exports = router;
