59ae2681154682d9bc5a6192526ce3d9a1ccb609
Adikso <adam@zambrzycki.net>
2020-05-25 15:24:15 +0200

    Change routing paths

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const controllers = require('../controllers/stations.controller');
 
 router.get('/stations', controllers.all);
 router.get('/stations/:id', controllers.findById);
-router.get('/stations/:id/measurements/latest', controllers.getLatestMeasurements);
-router.get('/stations/:id/measurements', controllers.getMeasurements);
+router.get('/stations/:id/measurements', controllers.getLatestMeasurements);
+router.get('/stations/:id/measurements/aggregated', controllers.getMeasurements);
 
 module.exports = router;
